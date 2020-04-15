6920299695e14919c5a21bb6870a6b15fe4ada31
gtauliarhmn <gusti7gusda@gmail.com>
2020-04-15 21:10:58 +0800

    Buat Class operasiString

--- src/string/operasistring.java
+++ src/string/operasistring.java
@@ -2,7 +2,7 @@ package string;
 
 public class operasiString {
     public static void main(String[] args) {
-        String kota = "yogyakarta";
+        String kota = "martapura";
 
         System.out.println(kota);
         char[] uniskaChar = { 'U','N','I','S','K','A'};
