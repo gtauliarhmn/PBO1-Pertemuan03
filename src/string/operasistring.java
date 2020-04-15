6d2f28335cfde02af0f449d1f0ca8155c5d90f9f
gtauliarhmn <gusti7gusda@gmail.com>
2020-04-15 21:04:27 +0800

    Buat Class operasiString

--- src/string/operasistring.java
+++ src/string/operasistring.java
@@ -1,10 +1,19 @@
-public class operasiString {
+package string;
+
+public class operasiStrin {
     public static void main(String[] args) {
-        String kota = "Martapura";
+        String kota = "banjarbaru";
 
         System.out.println(kota);
         char[] uniskaChar = { 'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        system.out.println(kota.toUpperCase());
+        system.out.println(kota.toLowerCase());
+        system.out.println(kota.substring(5));
+        system.out.println(kota.substring(0,4));
+
     }
 }
+
